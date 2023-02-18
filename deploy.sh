b414db7b1605bcd7133b5f1947fdd6e73bc2019a
york <york0704@gmail.com>
2023-02-18 21:24:12 +0800

    git init

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+以下擇一
+# if you are deploying to https://< github使用者名稱>.github.io
+# git push -f git@github.com:< github使用者名稱>/< github使用者名稱>.github.io.git main
+
+# if you are deploying to https://<github使用者名稱>.github.io/<REPO檔名> (建議使用)
+git push -f https://github.com/yehHsieh/<repo名>.git main:gh-pages (需全小寫)
+
+cd -
---
+++ src/views/AboutView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ data }}
+  </div>
+</template>
+
+<script >
+  export default{
+    data(){
+      return{
+        data:{}
+      }
+    },
+
+    mounted(){
+      console.log(import.meta.env.VITE_TEXT);
+      const url = import.meta.env.VITE_PATH;
+      this.$http.get(url)
+        .then((res)=>{
+          console.log(res);
+          this.data = res.data.results[0];
+          console.log(this.data) 
+        })
+    }
+  }
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
